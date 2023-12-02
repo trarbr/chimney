d9b372b913810ca13bb64c597e8e92719a253c99
Troels Brødsgaard <tr@arbr.dk>
2023-12-02 13:25:16 +0100

    Add mksquashfs_flags to drop the default -quiet flag
    The -quiet flag is not supported on my version of mksquashfs
    (4.3-git (2014/06/09)).

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,9 @@ config :chimney, target: Mix.target()
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
-config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+config :nerves, :firmware,
+  rootfs_overlay: "rootfs_overlay",
+  mksquashfs_flags: ["-no-xattrs"]
 
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
